79fe2efc06c91d9048b0df024e64df67dac43cdf
farnoosh <daarcparsa@yahoo.com>
2024-07-25 12:59:36 +0330

    https://github.com/Farnoosh-R/weblog.git

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,52 +1,55 @@
 
 
-// let url = window.location.href;
-// console.log(url.id)
-
 
 (function() {
     var retrievedObject = localStorage.getItem('arrayArticle');
     var data = JSON.parse(retrievedObject);
     console.log(data);
+    findArticle(data)
     showArticle(data);
  })();
 
- function showArticle(data){
+ function findArticle(data){
 
-    //const params = new URL(location.href).searchParams;
-    
 
-    //let url= new URL('https://javascriptjeep.com?mode=night&page=2');
     let url = window.location
     let params = new URLSearchParams(url.search);
     const paramId = params.get('article_id');
     console.log(params.get('article_id'));
+
+    let objContent = data.filter((p) => p.id === paramId);
     
-    
-    //data.forEach(item => {
-        //console.log(item.title)
-        let objContent = data.filter((p) => p.id === paramId);
-        console.log(objContent)
-        if (objContent.length === 1){
+    data[0].visit +=1;
+    localStorage.setItem('arrayArticle', JSON.stringify(data));
+    console.log('findArticle:' + data[0].visit);
+ }
+
+ function showArticle(data){
+
+              
+        if (data.length === 1){
             console.log('ok')
+
             let elContentBox = document.getElementById('content-box');
             let elContent = document.createElement('div');
             let elImg = document.createElement('img');
             elImg.src = "../assets/images/image1.jpg"
             let elTitle = document.createElement('h3');
+            let elVisit = document.createElement('p');
             let elBody = document.createElement('p');
 
-            elTitle.innerHTML = objContent[0].title;
-            elBody.innerHTML = objContent[0].body;
+            elTitle.innerHTML = `${data[0].title} :عنوان مطلب`
+            elBody.innerHTML = `${data[0].body} :متن`
+            elVisit.innerHTML = `تعداد بازدید: ${data[0].visit}`
             elContent.appendChild(elImg)
+            elContent.appendChild(elVisit)
             elContent.appendChild(elTitle)
+            
             elContent.appendChild(elBody)
             elContentBox.appendChild(elContent)
 
 
         }
-        
-  //  });
  }
 
 
