fabc0fc6ed39e063b8a11e687bc2b3da82f0f09f
farnoosh <daarcparsa@yahoo.com>
2024-07-27 10:12:27 +0330

    seventh commit

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,55 +1,50 @@
+(function () {
+  var retrievedObject = localStorage.getItem("arrayArticle");
+  var data = JSON.parse(retrievedObject);
+  console.log(data);
+  var objfiltered = findArticle(data);
+  showArticle(objfiltered);
+})();
 
+function findArticle(data) {
+  console.log("**");
+  console.log(data);
+  let url = window.location;
+  let params = new URLSearchParams(url.search);
+  const paramId = params.get("article_id");
+  console.log(params.get("article_id"));
 
+  let objContent = data.filter((p) => p.id === paramId);
 
-(function() {
-    var retrievedObject = localStorage.getItem('arrayArticle');
-    var data = JSON.parse(retrievedObject);
-    console.log(data);
-    findArticle(data)
-    showArticle(data);
- })();
+  objContent[0].visit += 1;
+  localStorage.setItem("arrayArticle", JSON.stringify(data));
+  console.log("findArticle:" + objContent[0].visit);
+  console.log(data);
+  return objContent;
+}
 
- function findArticle(data){
+function showArticle(data) {
+  console.log(data);
 
+  if (data.length === 1) {
+    console.log("ok");
 
-    let url = window.location
-    let params = new URLSearchParams(url.search);
-    const paramId = params.get('article_id');
-    console.log(params.get('article_id'));
+    let elContentBox = document.getElementById("content-box");
+    let elContent = document.createElement("div");
+    let elImg = document.createElement("img");
+    elImg.src = "../assets/images/image1.jpg";
+    let elTitle = document.createElement("h3");
+    let elVisit = document.createElement("p");
+    let elBody = document.createElement("p");
 
-    let objContent = data.filter((p) => p.id === paramId);
-    
-    data[0].visit +=1;
-    localStorage.setItem('arrayArticle', JSON.stringify(data));
-    console.log('findArticle:' + data[0].visit);
- }
+    elTitle.innerHTML = `${data[0].title} :عنوان مطلب`;
+    elBody.innerHTML = `${data[0].body} :متن`;
+    elVisit.innerHTML = `تعداد بازدید: ${data[0].visit}`;
+    elContent.appendChild(elImg);
+    elContent.appendChild(elVisit);
+    elContent.appendChild(elTitle);
 
- function showArticle(data){
-
-              
-        if (data.length === 1){
-            console.log('ok')
-
-            let elContentBox = document.getElementById('content-box');
-            let elContent = document.createElement('div');
-            let elImg = document.createElement('img');
-            elImg.src = "../assets/images/image1.jpg"
-            let elTitle = document.createElement('h3');
-            let elVisit = document.createElement('p');
-            let elBody = document.createElement('p');
-
-            elTitle.innerHTML = `${data[0].title} :عنوان مطلب`
-            elBody.innerHTML = `${data[0].body} :متن`
-            elVisit.innerHTML = `تعداد بازدید: ${data[0].visit}`
-            elContent.appendChild(elImg)
-            elContent.appendChild(elVisit)
-            elContent.appendChild(elTitle)
-            
-            elContent.appendChild(elBody)
-            elContentBox.appendChild(elContent)
-
-
-        }
- }
-
-
+    elContent.appendChild(elBody);
+    elContentBox.appendChild(elContent);
+  }
+}
